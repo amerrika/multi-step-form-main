3a557e7c727cc74d912f79230694b9f3b049e4bf
Amerrika <amer.tricic@live.com>
2023-01-09 14:58:19 +0100

    Update

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const inputPhone = reqFields[2];
 
 // STEP 1
 
-// Input email validation
+// Email Input Validation
 function validateEmail(mail) {
   // if entered text is ok  
   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail.value)) {
@@ -31,17 +31,23 @@ inputEmail.addEventListener("change", function (e) {
   validateEmail(inputEmail);
 });
 
-// Phone number validation
-
+// Phone Number Validation
 function validatePhone(number) {
-  if (isNaN(number.value)) {
-    alert("Your phone number is invalid");
-    // Clear the field
-    number.value = "";
-    return false;
-  } else {
-    return true;
-  }
+    // If input is not a digit
+    if (isNaN(number.value)) {
+        // Display the warning message
+        document.querySelector(".warning-phone").classList.remove("display-none");
+        document.querySelector(".warning-phone").textContent = "You have entered an invalid phone number!";
+        // Clear the field
+        number.value = "";
+        return false;
+    } 
+    // If input is ok
+    else {
+        // Remove potential warning message
+        document.querySelector(".warning-phone").classList.toggle("display-none");
+        return true;
+    }
 }
 
 inputPhone.addEventListener("change", function (e) {
@@ -49,6 +55,7 @@ inputPhone.addEventListener("change", function (e) {
   validatePhone(inputPhone);
 });
 
+// Button Next Operation
 btnNext.addEventListener("click", function (e) {
   // Prevent default form behaviour
   e.preventDefault();
@@ -61,5 +68,15 @@ btnNext.addEventListener("click", function (e) {
     // do your action
     step1El.classList.add("display-none");
     step2El.classList.remove("display-none");
+  } 
+  // if there is an empty field, display a warning message
+  else {
+    for(let i = 0; i < reqFields.length; i++){
+        if(reqFields[i].value == ""){
+            console.log("Field is empty");
+            console.log(reqFields[i].parentElement)
+            // reqFields[i].classList.remove("display-none")
+        }
+    }
   }
 });
