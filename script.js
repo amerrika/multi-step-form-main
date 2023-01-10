1e31ebc618e6dbc48dc8cb335d2d6dafd5505212
Amerrika <amer.tricic@live.com>
2023-01-10 17:04:32 +0100

    Small Update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const reqFields = document.getElementsByClassName("form-input");
 const btnNext = document.querySelector(".btn-next");
 const step1El = document.querySelector(".step-1-content");
 const step2El = document.querySelector(".step-2-content");
-const sidebar = document.querySelector(".sidebar")
+const sidebar = document.querySelector(".sidebar");
 const inputEmail = reqFields[1];
 const inputPhone = reqFields[2];
 
@@ -10,18 +10,18 @@ const inputPhone = reqFields[2];
 
 // Starting conditions
 
-function init(){
-    // Clear all input fields
-    for(let i = 0; i < reqFields.length; i++){
-        reqFields[i].value = "";
-    }
+function init() {
+  // Clear all input fields
+  for (let i = 0; i < reqFields.length; i++) {
+    reqFields[i].value = "";
+  }
 }
 
 init();
 
 // Email Input Validation
 function validateEmail(mail) {
-  // if entered text is ok  
+  // if entered text is ok
   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail.value)) {
     // remove potential warning message
     document.querySelector(".warning-email").classList.toggle("display-none");
@@ -31,7 +31,8 @@ function validateEmail(mail) {
   else {
     // Display the warning message
     document.querySelector(".warning-email").classList.remove("display-none");
-    document.querySelector(".warning-email").textContent = "You have entered an invalid email address!";
+    document.querySelector(".warning-email").textContent =
+      "You have entered an invalid email address!";
     // Clear the field
     mail.value = "";
     return false;
@@ -45,21 +46,22 @@ inputEmail.addEventListener("change", function (e) {
 
 // Phone Number Validation
 function validatePhone(number) {
-    // If input is not a digit
-    if (isNaN(number.value)) {
-        // Display the warning message
-        document.querySelector(".warning-phone").classList.remove("display-none");
-        document.querySelector(".warning-phone").textContent = "You have entered an invalid phone number!";
-        // Clear the field
-        number.value = "";
-        return false;
-    } 
-    // If input is ok
-    else {
-        // Remove potential warning message
-        document.querySelector(".warning-phone").classList.toggle("display-none");
-        return true;
-    }
+  // If input is not a digit
+  if (isNaN(number.value)) {
+    // Display the warning message
+    document.querySelector(".warning-phone").classList.remove("display-none");
+    document.querySelector(".warning-phone").textContent =
+      "You have entered an invalid phone number!";
+    // Clear the field
+    number.value = "";
+    return false;
+  }
+  // If input is ok
+  else {
+    // Remove potential warning message
+    document.querySelector(".warning-phone").classList.toggle("display-none");
+    return true;
+  }
 }
 
 inputPhone.addEventListener("change", function (e) {
@@ -81,20 +83,27 @@ btnNext.addEventListener("click", function (e) {
     step1El.classList.add("display-none");
     // Display Step 2
     step2El.classList.remove("display-none");
-  } 
+  }
   // if there is an empty field, display a warning message
   else {
-    for(let i = 0; i < reqFields.length; i++){
-        if(reqFields[i].value == ""){
-            reqFields[i].parentElement.firstElementChild.lastElementChild.classList.remove("display-none");
-        } else {
-            // Remove the warning message
-            reqFields[i].parentElement.firstElementChild.lastElementChild.classList.add("display-none")
-        }
+    for (let i = 0; i < reqFields.length; i++) {
+      if (reqFields[i].value == "") {
+        reqFields[
+          i
+        ].parentElement.firstElementChild.lastElementChild.classList.remove(
+          "display-none"
+        );
+      } else {
+        // Remove the warning message
+        reqFields[
+          i
+        ].parentElement.firstElementChild.lastElementChild.classList.add(
+          "display-none"
+        );
+      }
     }
   }
 });
-
 
 // STEP 2
 
@@ -105,22 +114,40 @@ btnNext.addEventListener("click", function (e) {
 const btnToggle = document.querySelector(".switch-toggle-button");
 const monPlans = document.getElementsByClassName("plan-info-m");
 const yearPlans = document.getElementsByClassName("plan-info-y");
-const extraInfos = document.getElementsByClassName("plan-info-extra")
+const extraInfos = document.getElementsByClassName("plan-info-extra");
 
-btnToggle.addEventListener("click", function(){
-  for(let i = 0; i < 3; i++){
+btnToggle.addEventListener("click", function () {
+  for (let i = 0; i < 3; i++) {
     yearPlans[i].classList.remove("display-none");
     extraInfos[i].classList.remove("display-none");
     monPlans[i].classList.add("display-none");
   }
-})
+});
 
-
-// getting infos (name, price) from selected plan;
+// getting infos (name and price) from selected plan;
 const planOptions = document.getElementsByClassName("plan-option");
+let planPrice;
+let planTitle;
 
-for(let i = 0; i < planOptions.length; i++){
-  planOptions[i].addEventListener("click", function(){
-    console.log(parseInt(planOptions[i].querySelector(".plan-price").textContent));
-  })
+for (let i = 0; i < planOptions.length; i++) {
+  planOptions[i].addEventListener("click", function () {
+    // yearly prices
+    if (
+      document.querySelector(".plan-info-m").classList.contains("display-none")
+    ) {
+      planPrice = parseInt(
+        planOptions[i].querySelectorAll(".plan-price")[1].textContent
+      );
+      planTitle = planOptions[i].querySelector(".tertiary-heading").textContent;
+      console.log(planPrice, planTitle);
+    } else {
+      // monthly prices
+      planPrice = parseInt(
+        planOptions[i].querySelectorAll(".plan-price")[0].textContent
+      );
+      planTitle = planOptions[i].querySelector(".tertiary-heading").textContent;
+      console.log(planPrice, planTitle);
+    }
+  });
 }
+
